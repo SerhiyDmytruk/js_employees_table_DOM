8710ee8e59d13a46d10806e54d7441e9c5375737
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-10-31 14:09:13 +0200

    Added logic for form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,16 +28,43 @@ function makeCopyToObj() {
   });
 }
 
+const classASC = '_asc';
+const classDESC = '_desc';
+
 function sorting(idx) {
-  const sorted = [...data].sort((a, b) => {
-    if (typeof a[idx] === 'string') {
-      return b[idx].localeCompare(a[idx]);
-    } else {
-      return b[idx] - a[idx];
-    }
-  });
+  let sorted;
+
+  if (sordedType()) {
+    sorted = [...data].sort((a, b) => {
+      if (typeof a[idx] === 'string') {
+        return b[idx].localeCompare(a[idx]);
+      } else {
+        return b[idx] - a[idx];
+      }
+    });
+  } else {
+    sorted = [...data].sort((a, b) => {
+      if (typeof a[idx] === 'string') {
+        return a[idx].localeCompare(b[idx]);
+      } else {
+        return a[idx] - b[idx];
+      }
+    });
+  }
 
   renderTable(sorted);
+}
+
+function sordedType() {
+  if (table.className.includes(classASC)) {
+    table.classList.remove(classASC);
+    table.classList.add(classDESC);
+  } else {
+    table.classList.remove(classDESC);
+    table.classList.add(classASC);
+  }
+
+  return table.className.includes(classASC);
 }
 
 function renderTable(obj) {
@@ -64,50 +91,128 @@ function renderTable(obj) {
   tbody.append(fragment);
 }
 
-const pushNotification = (posTop, posRight, title, description, type) => {
-  const div = document.createElement('div');
-  const h2 = document.createElement('h2');
-  const p = document.createElement('p');
+function activeRow() {
+  document.querySelectorAll('table tr').forEach((tr) => {
+    tr.classList.remove('active');
 
-  h2.textContent = title;
-  h2.className = 'title';
+    tr.addEventListener('click', () => {
+      document.querySelectorAll('table tr').forEach((row) => {
+        row.classList.remove('active');
+      });
 
-  p.textContent = description;
+      tr.classList.add('active');
+    });
+  });
+}
 
-  div.style.top = posTop + 'px';
-  div.style.right = posRight + 'px';
+activeRow();
 
-  div.className = `notification ${type}`;
-  div.append(h2);
-  div.append(p);
+function createForm() {
+  const form = document.createElement('form');
 
-  document.body.append(div);
+  form.classList.add('new-employee-form');
 
-//   setTimeout(function () {
-//     div.style.visibility = 'hidden';
-//   }, 2000);
-};
+  ['name', 'position', 'office', 'age', 'salary'].forEach((el) => {
+    const htmlTag = el === 'office' ? 'select' : 'input';
+    const formEl = document.createElement(htmlTag);
 
-pushNotification(
-  10,
-  10,
-  'Title of Success message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'success',
-);
+    if (el === 'name' || el === 'position') {
+      formEl.setAttribute('type', 'text');
+    }
 
-pushNotification(
-  150,
-  10,
-  'Title of Error message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'error',
-);
+    if (el === 'age' || el === 'salary') {
+      formEl.setAttribute('type', 'number');
+    }
 
-pushNotification(
-  290,
-  10,
-  'Title of Warning message',
-  'Message example.\n ' + 'Notification should contain title and description.',
-  'warning',
-);
+    if (el === 'office') {
+      [
+        'Tokyo',
+        'Singapore',
+        'London',
+        'New York',
+        'Edinburgh',
+        'San Francisco',
+      ].forEach((city) => {
+        const option = document.createElement('option');
+
+        option.textContent = city;
+        formEl.append(option);
+      });
+    }
+
+    formEl.setAttribute('name', el);
+    formEl.setAttribute('data-qa', el);
+    formEl.setAttribute('required', true);
+
+    const label = document.createElement('label');
+
+    label.textContent = el[0].toUpperCase() + el.substring(1) + ':';
+    label.append(formEl);
+    form.append(label);
+  });
+
+  const btn = document.createElement('button');
+
+  btn.textContent = 'Save to table';
+  form.append(btn);
+
+  document.body.append(form);
+
+  eventHandlertoForm();
+}
+
+createForm();
+
+function eventHandlertoForm() {
+  const pushNotification = (posTop, posRight, title, description, type) => {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+
+    h2.textContent = title;
+    h2.className = 'title';
+
+    p.textContent = description;
+
+    div.style.top = posTop + 'px';
+    div.style.right = posRight + 'px';
+
+    div.setAttribute('data-qa', 'notification');
+
+    div.className = `notification ${type}`;
+    div.append(h2);
+    div.append(p);
+
+    document.body.append(div);
+
+    setTimeout(function () {
+      div.style.visibility = 'hidden';
+    }, 2000);
+  };
+
+  document
+    .querySelector('.new-employee-form button')
+    .addEventListener('click', (e) => {
+      // e.preventDefault();
+
+      if (e.textContent !== 'success') {
+        pushNotification(
+          10,
+          10,
+          'Title of Success message',
+          'Message example.\n ' +
+            'Notification should contain title and description.',
+          'success',
+        );
+      } else {
+        pushNotification(
+          150,
+          10,
+          'Title of Error message',
+          'Message example.\n ' +
+            'Notification should contain title and description.',
+          'error',
+        );
+      }
+    });
+}
